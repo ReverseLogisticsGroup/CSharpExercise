0017992ebcc3a22b84595a595ef4544c0fdf5d16
Sebastian Bartosiak <sebastian.bartosiak@gmail.com>
2022-09-15 10:05:01 +0200

    Implemented RLGWhere and RLGSelect

--- src/CSharpExercise.Tests/RLGExtensions.Tests..cs
+++ src/CSharpExercise.Tests/RLGExtensions.Tests..cs
@@ -1,11 +1,75 @@
 namespace CSharpExercise.Tests;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 public class RLGExtensions_Tests
 {
 	[Fact]
-	public void Test1()
+	public void RLGWhere()
 	{
-		
+		var source = Enumerable.Range(1, 100);
+
+		var myResult = source.RLGWhere(x => x % 2 == 1);
+		var linqResult = source.Where(x => x % 2 == 1);
+
+		Assert.Equal(linqResult, myResult);
 	}
+
+    [Fact]
+    public void RLGWhereWithObject()
+    {
+        var source = Enumerable.Range(1, 100).Select(x => new {someValue = x});
+
+        var myResult = source.RLGWhere(x => x.someValue % 2 == 1);
+        var linqResult = source.Where(x => x.someValue % 2 == 1);
+
+        Assert.Equal(linqResult, myResult);
+    }
+    [Fact]
+    public void RLGWhereUsingSelectMany()
+    {
+        var source = Enumerable.Range(1, 10).Select(x => Enumerable.Range(1, 10));
+
+        var myResult = source.RLGWhere(x => x % 2 == 1);
+        var linqResult = source.SelectMany(x => x).Where(x => x % 2 == 1);
+
+        Assert.Equal(linqResult, myResult);
+    }
+    [Fact]
+    public void RLGSelect()
+    {
+        var source = Enumerable.Range(1, 100);
+
+        var myResult = source.RLGSelect(x => x * 2);
+        var linqResult = source.Select(x => x * 2);
+
+        Assert.Equal(linqResult, myResult);
+    }
+
+    [Fact]
+    public void RLGSelectWithObject()
+    {
+        var source = Enumerable.Range(1, 100).Select(x => new { someValue = x });
+
+        var myResult = source.RLGSelect(x => x.someValue*2 );
+        var linqResult = source.Select(x => x.someValue * 2);
+
+        Assert.Equal(linqResult, myResult);
+    }
+
+    [Fact]
+    public void RLGSelectUsingSelectMany()
+    {
+        var source = Enumerable.Range(1, 10).Select(x => Enumerable.Range(1,10));
+
+        var myResult = source.RLGSelect(x => x * 2);
+        var linqResult = source.SelectMany(x => x).Select(x => x * 2);
+
+        Assert.Equal(linqResult, myResult);
+    }
+
+
 }
--- src/CSharpExercise/RLGExtensions.cs
+++ src/CSharpExercise/RLGExtensions.cs
@@ -1,6 +1,111 @@
-﻿namespace CSharpExercise;
+﻿using System.Diagnostics;
+
+namespace CSharpExercise;
 
 public static class RLGExtensions
 {
+    public static IEnumerable<TSource> RLGWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException("source");
+        }
+        if (predicate == null)
+        {
+            throw new ArgumentNullException("predicate");
+        }
 
+        foreach (TSource item in source)
+        {
+            if(predicate(item))
+                yield return item;
+        }
+    }
+
+    public static IEnumerable<TSource> RLGWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException("source");
+        }
+        if (predicate == null)
+        {
+            throw new ArgumentNullException("predicate");
+        }
+        int index = 0;
+        foreach (TSource item in source)
+        {
+            if (predicate(item, index))
+                yield return item;
+            index++;
+        }
+    }
+
+    public static IEnumerable<TResult> RLGSelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException("source");
+        }
+        if (selector == null)
+        {
+            throw new ArgumentNullException("selector");
+        }
+        foreach (TSource item in source)
+        {
+            yield return selector(item);
+        }
+    }
+
+    public static IEnumerable<TResult> RLGSelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException("source");
+        }
+        if (selector == null)
+        {
+            throw new ArgumentNullException("selector");
+        }
+        int index = 0;
+        foreach (TSource item in source)
+        {
+            yield return selector(item, index);
+            index++;
+        }
+    }
+
+    public static IEnumerable<TSource> RLGWhere<TSource>(this IEnumerable<IEnumerable<TSource>> source, Func<TSource, bool> predicate)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException("source");
+        }
+        if (predicate == null)
+        {
+            throw new ArgumentNullException("predicate");
+        }
+
+        foreach (TSource item in source.SelectMany(x => x))
+        {
+            if (predicate(item))
+                yield return item;
+        }
+    }
+
+    public static IEnumerable<TResult> RLGSelect<TSource, TResult>(this IEnumerable<IEnumerable<TSource>> source, Func<TSource, TResult> selector)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException("source");
+        }
+        if (selector == null)
+        {
+            throw new ArgumentNullException("selector");
+        }
+        foreach (TSource item in source.SelectMany(x => x))
+        {
+            yield return selector(item);
+        }
+    }
 }
