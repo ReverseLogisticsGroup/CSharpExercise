00e66357c62c40df394fab51f7a187cb44f90e7e
Alin Popa <alin.popa@live.com>
2022-09-15 12:36:05 +0300

    Implementation of extension methods: Where and Select from IEnumerable plus Unit Tests

--- src/CSharpExercise.Tests/RLGExtensions.Tests..cs
+++ src/CSharpExercise.Tests/RLGExtensions.Tests..cs
@@ -1,11 +1,52 @@
-namespace CSharpExercise.Tests;
 using Xunit;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
-public class RLGExtensions_Tests
+namespace CSharpExercise.Tests
 {
-	[Fact]
-	public void Test1()
-	{
-		
-	}
+    public class RLGExtensions_Tests
+    {
+
+        bool positive(int x, int y)
+        {
+            return x > 0;
+        }
+
+
+        IEnumerable<int> FillEnumerable(int x)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < x; i++)
+            {
+                result.Add(i);
+            }
+
+            return result;
+        }
+
+        [Fact]
+        public void RLGSelectUsingSelectMany()
+        {
+            var source = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var rlgResult = source.RLGSelect(x => x);
+            var linqResult = source.SelectMany(x => FillEnumerable(10)).Select(x => x);
+
+            Assert.Equal(linqResult, rlgResult);
+        }
+
+        [Fact]
+        public void RLGWhereUsingSelectMany()
+        {
+            var source = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 };
+
+
+            var rlgResult = source.RLGWhere(positive);
+            var linqResult = source.SelectMany(x => FillEnumerable(10)).Where(positive);
+
+            Assert.Equal(linqResult, rlgResult);
+        }
+
+    }
 }
--- src/CSharpExercise/RLGExtensions.cs
+++ src/CSharpExercise/RLGExtensions.cs
@@ -1,6 +1,51 @@
-﻿namespace CSharpExercise;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public static class RLGExtensions
+namespace CSharpExercise
 {
 
+    public static class RLGExtensions
+    {
+        public static IEnumerable<TSource> RLGWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new Exception("soruce is null");
+            }
+
+            if (predicate == null)
+            {
+                throw new Exception("predicate is null");
+            }
+
+            int index = 0;
+            foreach (TSource item in source)
+            {
+                if (predicate(item, index))
+                    yield return item;
+                index++;
+            }
+        }
+
+        public static IEnumerable<TSource> RLGSelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            if (source == null)
+            {
+                throw new Exception("source is null");
+            }
+
+            if (selector == null)
+            {
+                throw new Exception("selector is null");
+            }
+
+            foreach (TSource item in source.SelectMany(x => source))
+            {
+                yield return item;
+            }
+        }
+
+
+    }
 }
